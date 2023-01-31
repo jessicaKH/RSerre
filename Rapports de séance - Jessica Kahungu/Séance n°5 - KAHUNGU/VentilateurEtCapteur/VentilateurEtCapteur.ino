c89624aa6559c94be03d58e107cf2ca51512362e
jessicaKH <120385973+jessicaKH@users.noreply.github.com>
2023-01-31 14:39:24 +0100

    Add files via upload

---
+++ Rapports de séance - Jessica Kahungu/Séance n°5 - KAHUNGU/VentilateurEtCapteur/VentilateurEtCapteur.ino
@@ -0,0 +1,65 @@
+// inclure les librairies
+#include <DHT.h> // librairie pour le capteur de température DHT22
+#include <LiquidCrystal_I2C.h> // librairie pour l'écran LCD
+#define DHTPIN 2 // Definit la broche de l'Arduino sur laquelle la broche DATA du capteur est reliee -> broche 2 
+#define DHTTYPE DHT22 // Definit le type de capteur utilisé
+
+#include<SoftwareSerial.h>
+#define RX 10
+#define TX 11
+SoftwareSerial BlueT(RX,TX);
+int mess;
+
+DHT dht(DHTPIN,DHTTYPE); // Déclare un objet de type DHT, Il faut passer en parametre du constructeur de l'objet la broche et le type de capteur
+
+#define ventilateurPIN 1 // broche 1 de l'arduino -> pour ventilateur 
+LiquidCrystal_I2C lcd(0x27,16,2); //16 colonnes et 2 lignes pour l'écran LCD
+
+void setup(){  
+  pinMode(ventilateurPIN,OUTPUT); //règle broche 1 de l'arduino en mode sortie
+  lcd.init(); // initialisation de l’afficheur 
+  lcd.backlight(); //lumière pour l'écran
+  dht.begin(); // Initialise la capteur DHT22
+  Serial.begin(9600);
+  delay(500);
+  Serial.println("Bonjour -Pret pour les commandes AT");
+  BlueT.begin(9600);
+  delay(500);
+} 
+
+void loop(){
+  
+  float t = dht.readTemperature(); // récupération de la température
+  float h = dht.readHumidity();
+
+  while (BlueT.available()>0) {
+    mess=BlueT.read();
+  }  
+
+  BlueT.println("temp");
+  BlueT.println(t);
+  BlueT.println("hum");
+  BlueT.println(h);
+
+  Serial.println("Temperature");
+  Serial.println(t);
+  Serial.println("HumAir");
+  Serial.println(h);
+  
+  //............
+  lcd.clear(); // initialisation de l'écran LCD  
+  lcd.setCursor(2,0); //initilisation des lignes et colonnes de l'écran lCD -> affichage température   
+  lcd.print("temperature : "); 
+  lcd.setCursor(2,1);
+  lcd.print(t);  
+  lcd.print(" degres"); 
+  //............
+   
+  if (t > 10) { // si la température est supérieure à 25 degrés, on active le ventilateur
+    digitalWrite(ventilateurPIN,HIGH); // le ventilateur se met à tourner 
+  } else { // si la température est est inférieure ou égale à 25 degrés, on éteins le ventilateur
+    digitalWrite(ventilateurPIN,LOW); // le ventilateur s'arrête
+  
+  } 
+  delay(2000);  // Attend 2 secondes avant de reboucler
+} 
